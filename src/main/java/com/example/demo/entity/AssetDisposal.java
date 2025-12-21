4c042571b5a506df93cb0594dd4789441d972f15
bala-amypo <bala@amypo.in>
2025-12-21 15:41:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssetDisposal.java
+++ src/main/java/com/example/demo/entity/AssetDisposal.java
@@ -19,4 +19,47 @@ public class AssetDisposal {
     private LocalDate disposalDate;
     private LocalDateTime createdAt;
 
+    public Long getId() {
+        return id;
+    }
+
+    public Asset getAsset() {
+        return asset;
+    }
+
+    public void setAsset(Asset asset) {
+        this.asset = asset;
+    }
+
+    public String getDisposalMethod() {
+        return disposalMethod;
+    }
+
+    public void setDisposalMethod(String disposalMethod) {
+        this.disposalMethod = disposalMethod;
+    }
+
+    public Double getDisposalValue() {
+        return disposalValue;
+    }
+
+    public void setDisposalValue(Double disposalValue) {
+        this.disposalValue = disposalValue;
+    }
+
+    public LocalDate getDisposalDate() {
+        return disposalDate;
+    }
+
+    public void setDisposalDate(LocalDate disposalDate) {
+        this.disposalDate = disposalDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }

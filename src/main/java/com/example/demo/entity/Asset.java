e4b908e92178c6d2450637370affefc5c8155957
bala-amypo <bala@amypo.in>
2025-12-21 18:07:19 +0000

    User commit

--- src/main/java/com/example/demo/entity/Asset.java
+++ src/main/java/com/example/demo/entity/Asset.java
@@ -3,19 +3,37 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Entity
+@Table(name = "assets")
 public class Asset {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
+    private String assetTag;
+
     private String assetName;
 
+    private LocalDate purchaseDate;
+
+    private Double purchaseCost;
+
     private String status;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Vendor vendor;
+
+    @ManyToOne
+    private DepreciationRule depreciationRule;
 }

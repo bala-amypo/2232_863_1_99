f0fd0efa9be788515f9e31fa3b1ea919274f2bba
bala-amypo <bala@amypo.in>
2025-12-24 08:18:04 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtFilter.java
+++ src/main/java/com/example/demo/config/JwtFilter.java
@@ -1,3 +1,21 @@
+package com.example.demo.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.filter.OncePerRequestFilter;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 

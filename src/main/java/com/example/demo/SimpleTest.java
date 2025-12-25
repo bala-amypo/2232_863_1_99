80fd7e1ee3ae2f0c9a90dc3de3020d2576e156e2
bala-amypo <bala@amypo.in>
2025-12-25 16:25:13 +0000

    User commit

--- src/main/java/com/example/demo/SimpleTest.java
+++ src/main/java/com/example/demo/SimpleTest.java
@@ -1,21 +1,19 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.Test;
 
 @SpringBootTest
-public class SimpleTest extends AbstractTestNGSpringContextTests {
+public class SimpleTest {
     
     @Test
     public void contextLoads() {
         // This test will pass if the Spring context loads successfully
-        assert true;
     }
     
     @Test
     public void basicTest() {
-        // Simple test to verify TestNG is working
+        // Simple test to verify JUnit is working
         assert 1 + 1 == 2;
     }
 }

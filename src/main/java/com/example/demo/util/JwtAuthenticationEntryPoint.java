8405bcb7c5ce22e674eca9d842997ac17dfa0b29
bala-amypo <bala@amypo.in>
2025-12-26 10:22:59 +0000

    User commit

--- src/main/java/com/example/demo/util/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/demo/util/JwtAuthenticationEntryPoint.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import com.example.demo.util.JwtFilter;
+import com.example.demo.util.JwtAuthenticationEntryPoint;
 
 import java.io.IOException;
 

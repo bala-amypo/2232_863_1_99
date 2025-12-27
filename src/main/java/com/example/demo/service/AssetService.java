0ce95815a6b3503cc6f4788bcf66ec2168db55c3
bala-amypo <bala@amypo.in>
2025-12-27 05:32:10 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetService.java
+++ src/main/java/com/example/demo/service/AssetService.java
@@ -1,57 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.*;
-import com.example.demo.repository.*;
-import jakarta.persistence.EntityNotFoundException;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
+import com.example.demo.entity.Asset;
 
-@Service
-public class AssetService {
+public interface AssetService {
 
-    private final AssetRepository assetRepository;
-    private final VendorRepository vendorRepository;
-    private final DepreciationRuleRepository depreciationRuleRepository;
-
-    public AssetService(
-            AssetRepository assetRepository,
-            VendorRepository vendorRepository,
-            DepreciationRuleRepository depreciationRuleRepository) {
-
-        this.assetRepository = assetRepository;
-        this.vendorRepository = vendorRepository;
-        this.depreciationRuleRepository = depreciationRuleRepository;
-    }
-
-    public Asset createAsset(
+    Asset createAsset(
             Asset asset,
             Long vendorId,
-            Long depreciationRuleId) {
-
-        if (asset.getPurchaseCost() != null &&
-                asset.getPurchaseCost() < 0) {
-            throw new IllegalArgumentException(
-                    "Purchase cost must be positive");
-        }
-
-        if (assetRepository.existsByAssetTag(asset.getAssetTag())) {
-            throw new DataIntegrityViolationException(
-                    "Duplicate asset tag");
-        }
-
-        Vendor vendor = vendorRepository.findById(vendorId)
-                .orElseThrow(() ->
-                        new EntityNotFoundException("Vendor not found"));
-
-        DepreciationRule rule =
-                depreciationRuleRepository.findById(depreciationRuleId)
-                .orElseThrow(() ->
-                        new EntityNotFoundException("Rule not found"));
-
-        asset.setVendor(vendor);
-        asset.setDepreciationRule(rule);
-        asset.setStatus("ACTIVE");
-
-        return assetRepository.save(asset);
-    }
+            Long depreciationRuleId
+    );
 }

698a577d13b24a52238f97ab1856432c4c012615
bala-amypo <bala@amypo.in>
2025-12-28 06:56:38 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetDisposalService.java
+++ src/main/java/com/example/demo/service/AssetDisposalService.java
@@ -1,6 +1,9 @@
 package com.example.demo.service;
+
 import com.example.demo.entity.AssetDisposal;
+
 public interface AssetDisposalService {
+
     AssetDisposal requestDisposal(Long assetId, AssetDisposal disposal);
     AssetDisposal approveDisposal(Long disposalId, Long adminId);
 }

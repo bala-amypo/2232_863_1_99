72f248596dea1d6e42a686eb2e86a8b4e40fc708
bala-amypo <bala@amypo.in>
2025-12-29 15:50:21 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetLifecycleEventService.java
+++ src/main/java/com/example/demo/service/AssetLifecycleEventService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
+
 import com.example.demo.entity.AssetLifecycleEvent;
 import java.util.List;
+
 public interface AssetLifecycleEventService {
     AssetLifecycleEvent logEvent(Long assetId, AssetLifecycleEvent event);
-    List<AssetLifecycleEvent> getEvents(Long assetId);
+    List<AssetLifecycleEvent> getEventsForAsset(Long assetId);
 }

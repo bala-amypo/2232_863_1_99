cbff547f2e4cec2791dde865fc8b62cc25c9bcff
bala-amypo <bala@amypo.in>
2025-12-29 15:51:08 +0000

    User commit

--- src/main/java/com/example/demo/service/DepreciationRuleService.java
+++ src/main/java/com/example/demo/service/DepreciationRuleService.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
+
 import com.example.demo.entity.DepreciationRule;
 import java.util.List;
+
 public interface DepreciationRuleService {
     DepreciationRule createRule(DepreciationRule rule);
     List<DepreciationRule> getAllRules();

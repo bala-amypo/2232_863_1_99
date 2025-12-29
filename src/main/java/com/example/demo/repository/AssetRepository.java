e057dfe5fb2c2628bc352205f8c4b57465ff195d
bala-amypo <bala@amypo.in>
2025-12-29 15:45:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssetRepository.java
+++ src/main/java/com/example/demo/repository/AssetRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo.repository;
+
 import com.example.demo.entity.Asset;
 import com.example.demo.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+
 public interface AssetRepository extends JpaRepository<Asset, Long> {
-    boolean existsByAssetTag(String assetTag);
-    List<Asset> findByStatus(String status);
-    List<Asset> findByVendor(Vendor vendor);
+    boolean existsByAssetTag(String assetTag); // [cite: 711]
+    List<Asset> findByStatus(String status);   // [cite: 712]
+    List<Asset> findByVendor(Vendor vendor);   // [cite: 713]
 }
